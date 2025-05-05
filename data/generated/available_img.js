080d5e408093bcb4c7eb06050c808bc23c8930c7
qingqiantu <tqq94666@163.com>
2025-05-06 05:53:25 +0800

    Add files via upload

---
+++ data/generated/available_img.js
@@ -0,0 +1,1 @@
+const availableImg = ["Banerjee2012Coalition","Beck2016Visual","Becker2021Achievement","DeTroyer2019TrueBiters","MayerEichberger2020QBFGames","Schaefer2013LogicGame","Scholl2018DQBF","Schuppan2018Cores","Shaik2023SQval","Shukla2019QBF","Weng2010PacmanLogic"];
